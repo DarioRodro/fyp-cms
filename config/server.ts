7fbb99e40f707a9949273e720bc31bef4186c01c
unknown <dariorodq30@gmail.com>
2025-06-23 02:00:41 -0500

    fix: update port and public url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
-  url: env('PUBLIC_URL', 'https://fyp-cms.up.railway.app'),
+  url: env('PUBLIC_URL', 'https://fyp-cms-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
