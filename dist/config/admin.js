107b11fd9b54490d3afad030745ca97f4075a2fc
unknown <dariorodq30@gmail.com>
2025-06-24 00:51:31 -0500

    fix: recompile dist and allow deploy

---
+++ dist/config/admin.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+    },
+    apiToken: {
+        salt: env('API_TOKEN_SALT'),
+    },
+    transfer: {
+        token: {
+            salt: env('TRANSFER_TOKEN_SALT'),
+        },
+    },
+    secrets: {
+        encryptionKey: env('ENCRYPTION_KEY'),
+    },
+    flags: {
+        nps: env.bool('FLAG_NPS', true),
+        promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    },
+    /**
+     * ESTA PARTE es la que falta para que Railway lo sirva correctamente:
+     */
+    url: '/admin',
+    serveAdminPanel: true,
+});
---
+++ dist/config/middlewares.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: ['http://localhost:4200'], // tu app Angular
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+            headers: '*',
+            keepHeaderCase: true,
+        },
+    },
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+];
