aec303197a2ac2b8cfbbdbc632c5caa8022040bc
unknown <dariorodq30@gmail.com>
2025-06-24 02:02:41 -0500

    build después de configurar Cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,4 +4,14 @@ export default ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
     },
   },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('dmflxxhkn'),
+        api_key: env('761547111135628'),
+        api_secret: env('__vBfyevMgSUgPRHBUk2tOrpS3Y'),
+      },
+    },
+  },
 });
--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -6,4 +6,14 @@ exports.default = ({ env }) => ({
             jwtSecret: env('JWT_SECRET'),
         },
     },
+    upload: {
+        config: {
+            provider: '@strapi/provider-upload-cloudinary',
+            providerOptions: {
+                cloud_name: env('dmflxxhkn'),
+                api_key: env('761547111135628'),
+                api_secret: env('__vBfyevMgSUgPRHBUk2tOrpS3Y'),
+            },
+        },
+    },
 });
