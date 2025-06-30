d9199d7aea194f367666f1c1c6ade36710984ff7
unknown <dariorodq30@gmail.com>
2025-06-29 23:21:40 -0500

    fix: mejor configuración de Cloudinary para mostrar miniaturas

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,6 +12,15 @@ export default ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {
+          folder: 'fidy', // opcional, ponle un nombre si quieres agrupar tus imágenes
+          use_filename: true,
+          unique_filename: false,
+          overwrite: true,
+        },
+        delete: {},
+      },
     },
   },
 });
